0a2ef6036f6a8c1f121c582f4f530a98b160910e
vspdontukurthi <vsp@hotmail.co.uk>
2017-12-18 16:14:50 +0000

    satya.prakash:update tests.

--- src/test/java/com/stratengllc/bdd/runner/RunCalculatorTest.java
+++ src/test/java/com/stratengllc/bdd/runner/RunCalculatorTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-	format = {"pretty", "html:target/cucumber-htmlreport","json:target/cucumber-report.json"},
+	format = {"pretty", "html:target/cucumber-htmlreport","json:cucumber-report.json"},
         glue = "com.stratengllc.bdd.steps",
         features = "classpath:cucumber/calculator.feature"
 )
--- src/test/java/com/stratengllc/bdd/steps/CalculatorSteps.java
+++ src/test/java/com/stratengllc/bdd/steps/CalculatorSteps.java
@@ -1,13 +1,14 @@
 package com.stratengllc.bdd.steps;
  
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import com.stratengllc.bdd.calculator.Calculator;
+
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
- 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
  
 public class CalculatorSteps {
  
